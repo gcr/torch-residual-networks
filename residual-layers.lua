33c64d8a278c3e96914073ea88b6638ed0a3fc6e
Michael Wilber <wilmic1102@gmail.com>
2015-12-31 21:26:45 +0000

    Add padding / sampling to the residual layers. Now I can rip out my own homegrown junk.

--- residual-layers.lua
+++ residual-layers.lua
@@ -1,19 +1,32 @@
 require 'nn'
 require 'nngraph'
 require 'cudnn'
+require 'cunn'
 
-function addResidualLayer2(input,  nChannels)
-   -- Downsampling and convolution path
-   local net = cudnn.SpatialConvolution(nChannels, nChannels,
-                                           3,3, 1,1, 1,1)(input)
+function addResidualLayer2(input,  nChannels, nOutChannels, stride)
+   nOutChannels = nOutChannels or nChannels
+   stride = stride or 1
+   -- Path 1: Convolution
+   -- The first layer does the downsampling and the striding
+   local net = cudnn.SpatialConvolution(nChannels, nOutChannels,
+                                           3,3, stride,stride, 1,1)(input)
    net = cudnn.ReLU(true)(net)
-   net = cudnn.SpatialConvolution(nChannels, nChannels,
+   net = cudnn.SpatialConvolution(nOutChannels, nOutChannels,
                                       3,3, 1,1, 1,1)(net)
-   -- Add them together
-   --return net
-   return nn.CAddTable(){net, input}
-end
+   -- Path 2: Identity / skip connection
+   local skip = input
+   if stride > 1 then
+       -- optional downsampling
+       skip = nn.SpatialAveragePooling(1, 1, stride,stride)(skip)
+   end
+   if nOutChannels > nChannels then
+       -- optional padding
+       skip = nn.Padding(1, (nOutChannels - nChannels), 3)(skip)
+   end
 
+   -- Add them together
+   return nn.CAddTable(){net, skip}
+end
 
 function addResidualLayer3(input,  inChannels, hiddenChannels, outChannels)
    -- Downsampling and convolution path
@@ -32,11 +45,10 @@ function addResidualLayer3(input,  inChannels, hiddenChannels, outChannels)
    return nn.CAddTable(){net, input}
 end
 
--- input = nn.Identity()()
--- net = addResidualLayer(input, 256,64,256)
--- net = nn.gModule({input}, {net})
--- graph.dot(net.fg, "MLP")
 
+
+--[[
+-- Useful for memory debugging
 
 function countElts(modules)
    local sum_elts = 0
@@ -68,11 +80,12 @@ function accumMemoryByFieldName(module, accum)
       end
    end
 end
+--]]
 
---[[
+-- --[[
 -- Testing
 input = nn.Identity()()
-output = addResidualLayer2(input, 3)
+output = addResidualLayer2(input, 3, 6,  2)
 net = nn.gModule({input},{output})
 
 i = torch.randn(1, 3, 5,5):fill(1):cuda()
@@ -84,7 +97,7 @@ net.modules[2].weight:fill(0)
 net.modules[4].weight:fill(0)
 --]]
 
--- -- Let's make some!
+-- -- Testing memory usage
 -- i = torch.randn(5, 256, 224,224)
 -- o = net:forward(i)
 -- net:backward(i, o)
