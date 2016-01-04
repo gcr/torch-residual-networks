7947d153b50210fc363d02f1ded57e9060c61cca
Michael Wilber <wilmic1102@gmail.com>
2016-01-04 01:04:19 +0000

    Multi GPU training beginning

--- train-imagenet.lua
+++ train-imagenet.lua
@@ -8,8 +8,8 @@ require 'train-helpers'
 display = require 'display'
 
 opt = lapp[[
-      --batchSize       (default 24)      Sub-batch size
-      --iterSize        (default 8)       How many sub-batches in each batch
+      --batchSize       (default 96)      Sub-batch size
+      --iterSize        (default 1)       How many sub-batches in each batch
       --nThreads        (default 4)       Data loader threads
       --dataTrainRoot   (default /mnt/imagenet/train)   Data root folder
       --dataValRoot     (default /mnt/imagenet/val)   Data root folder
@@ -97,6 +97,17 @@ loss = nn.ClassNLLCriterion()
 model:cuda()
 loss:cuda()
 
+-- Convert model to multi-GPU model :)
+dmodel = nn.DataParallelTable(1)
+for i=1, 4 do
+    cutorch.setDevice(i)
+    dmodel:add(model:clone():cuda(), i)
+end
+cutorch.setDevice(1)
+model = dmodel
+-- required to fix call to SyncParameters
+model:forward(torch.randn(8, 3, 224,224):cuda())
+
 -- Dirty trick: make the first conv layer weights easier to modify
 -- model.modules[2].weight:mul(0.5)
 
@@ -131,7 +142,7 @@ print(mem_usage)
 
 sgdState = {
    --- For SGD with momentum ---
-   -- --[[
+   ----[[
    -- My semi-working settings
    learningRate = 0.001,
    weightDecay    = 1e-4,
@@ -145,7 +156,7 @@ sgdState = {
    --]]
    --- For rmsprop, which is very fiddly and I don't trust it at all ---
    --[[
-   learningRate = 1e-8,
+   learningRate = 1e-5,
    alpha = 0.9,
    whichOptimMethod = 'rmsprop',
    --]]
@@ -176,8 +187,16 @@ sgdState = {
 -- Actual Training! -----------------------------
 weights, gradients = model:getParameters()
 function forwardBackwardBatch(batch)
+    -- After every batch, the different GPUs all have different gradients
+    -- (because they saw different data), and only the first GPU's weights were
+    -- actually updated.
+    -- We have to do two changes:
+    --   - Copy the new parameters from GPU #1 to the rest of them;
+    --   - Zero the gradient parameters so we can accumulate them again.
    model:training()
-   gradients:zero()
+   model:syncParameters() -- This copies parameters from first GPU to
+   --gradients:zero() -- Not enough!! This will only zero GPU #1's params!
+   model:zeroGradParameters()
 
    --[[
    if sgdState.nSampledImages < 10000 then
@@ -198,12 +217,13 @@ function forwardBackwardBatch(batch)
        loss_val = loss_val + loss:forward(y, labels)
        local df_dw = loss:backward(y, labels)
        model:backward(inputs, df_dw)
+       -- The above call will accumulate all GPUs' parameters onto GPU #1
    end
    loss_val = loss_val / N
    gradients:mul( 1.0 / N )
 
    if sgdState.nEvalCounter % 20 == 0 then
-      display.image(model.modules[2].weight, {win=24, title="First layer weights"})
+      display.image(model.modules[1].modules[2].weight, {win=24, title="First layer weights"})
    end
 
    return loss_val, gradients, inputs:size(1) * N
@@ -230,7 +250,7 @@ os.execute('convert /tmp/MLP.svg /tmp/MLP.png')
 display.image(image.load('/tmp/MLP.png'), {title="Network Structure", win=23})
 --]]
 
--- --[[
+--[[
 require 'ncdu-model-explore'
 local y = model:forward(torch.randn(opt.batchSize, 3, 224,224):cuda())
 local df_dw = loss:backward(y, torch.zeros(opt.batchSize):cuda())
@@ -239,7 +259,7 @@ exploreNcdu(model)
 --]]
 
 
----[[
+-- --[[
 TrainingHelpers.trainForever(
    model,
    forwardBackwardBatch,
